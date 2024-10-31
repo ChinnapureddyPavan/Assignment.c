6eaeb9ecfff33370f96f2c9d787f2a5146641b35
ChinnapureddyPavan <u24ai033@aid.svnit.ac.in>
2024-10-31 11:35:57 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int evenorodd(int n)
+    {
+        if(n%2==0)
+        printf("entered number is even");
+        else
+        printf("entered number is odd ");
+    }
+    int n;
+    evenorodd(5);
+}
